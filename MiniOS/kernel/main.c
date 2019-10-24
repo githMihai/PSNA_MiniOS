5610632e4ad3a105274376a1024e6acf6b5b9993
Mihai-Adrian <m.deac.adrian@gmail.com>
2019-10-25 02:01:53 +0300

    Mini OS original

---
+++ MiniOS/kernel/main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "screen.h"
+
+void KernelMain()
+{
+    //__magic();    // break into BOCHS
+    
+    __enableSSE();  // only for demo; in the future will be called from __init.asm
+
+    ClearScreen();
+
+	HelloBoot();
+
+    // TODO!!! PIC programming; see http://www.osdever.net/tutorials/view/programming-the-pic
+    // TODO!!! define interrupt routines and dump trap frame
+    
+    // TODO!!! Timer programming
+
+    // TODO!!! Keyboard programming
+
+    // TODO!!! Implement a simple console
+
+    // TODO!!! read disk sectors using PIO mode ATA
+
+    // TODO!!! Memory management: virtual, physical and heap memory allocators
+}
